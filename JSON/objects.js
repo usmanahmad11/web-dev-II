1fd69b6191aaed62ac15487398903c832f4f5115
Usman Ahmad <usmanahmad50@outlook.com>
2023-12-10 20:11:04 +0400

    d

--- JSON/objects.js
+++
@@ -1,8 +0,0 @@
-const student = {
-    name: "Mark",
-    email: "abc@gmail.com",
-    program: "CC",
-    location:"Dubai"
-}
-
-console.log(student.email);
--- Vue/app.js
+++
@@ -1,5 +0,0 @@
-const app = Vue.createApp({
-    template: '<h2> I am template<h2>'
-})
-
-app.mount('#app')
