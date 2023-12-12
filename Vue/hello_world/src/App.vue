91636861a7feaedab90aac3d8ac2dfcb4c1f0554
Usman Ahmad <usmanahmad50@outlook.com>
2023-12-12 20:08:42 +0400

    fgfd

--- Vue/hello_world/src/App.vue
+++ Vue/hello_world/src/App.vue
@@ -1,20 +1,17 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script >
+export default {
+  name:"App",
+  data() {
+    return {
+      name:"Usman Ahmad"
+    }
+  }
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>Hello {{ name }}</div>
 </template>
 
 <style scoped>
